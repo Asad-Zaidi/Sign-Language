73b70583f74f6824603048d5bfaf50a5b61f4440
Syed Asad Jamil <syedasad1410@gmail.com>
2024-12-08 13:19:37 +0500

    Sign Language

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='signLanguage',
+    version='0.0.0',
+    author= 'Asad',
+    author_email='syedasad1410@gmail.com',
+    description='Sign Language Recognition',
+    packages=find_packages(),
+    install_requires=[]
+)
